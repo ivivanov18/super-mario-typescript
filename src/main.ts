66a73d2ddd40db8ff2794f7c8a2c03523010e263
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2021-06-16 22:33:49 +0300

    feat: - add function to load image - add tileset in img directory

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 const canvas = <HTMLCanvasElement>document.getElementById("screen");
 const context = canvas.getContext("2d");
-
 context.fillRect(0, 0, 50, 50);
+
+loadImage("/img/tileset.png").then((img) => {
+  context.drawImage(img, 0, 0);
+});
+
+function loadImage(src: string): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+    const image: HTMLImageElement = new Image();
+    image.addEventListener("load", () => {
+      resolve(image);
+    });
+    image.src = src;
+  });
+}

ab0ce2b3837a7596fcd7e039202489395ead08d9
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2021-06-16 22:55:47 +0300

    feat: - add utils class Vector2

---
+++ src/utils/vector.ts
@@ -0,0 +1,25 @@
+class Vector2 {
+  private _x: number;
+  private _y: number;
+
+  constructor(x: number = 0, y: number = 0) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(value: number) {
+    this._x = value;
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  set y(value: number) {
+    this._y = value;
+  }
+}
